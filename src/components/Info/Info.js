e88a24a3a482a03607381a40df0dc7eff6d74624
mosraful21 <mosraful21@gmail.com>
2022-10-26 02:23:14 +0600

    imformation images set

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -6,7 +6,10 @@ const Info = () => {
     return (
         <div className='w-4/5 m-auto mt-12 border'>
             <h1 className='text-3xl text-center font-semibold'>Course Name: {info.title}</h1>
-            <p className='p-12 text-2xl'> <span className='font-semibold'>Details:</span> {info.details}</p>
+            <div className='flex justify-between'>
+                <img className='w-72 h-72 ml-8 mt-4' src={info.image} alt="" />
+                <p className='p-8 text-2xl text-justify'> <span className='font-semibold'>Details:</span> {info.details}</p>
+            </div>
             <div className='grid justify-items-center mb-4'>
                 <button className="btn btn-active btn-accent">Add to cart</button>
             </div>
